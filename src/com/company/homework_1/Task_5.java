275f9d38695144f8daf1f6681071b9631ca9474f
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2019-10-08 22:02:38 +0300

    task5

---
+++ src/com/company/homework_1/Task_5.java
@@ -0,0 +1,15 @@
+package com.company.homework_1;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        double sm,dm;
+        int counter=0;
+
+        for (dm = 1; dm <21 ; dm++) {
+            sm=dm/2.54;
+            System.out.println(dm+" Dyims corresponds "+sm+" Santimetres");
+            counter++;
+        }
+
+    }
+}
