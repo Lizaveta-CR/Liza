ce8ec3cab6f7485b37b9d2513c914fb5ff1e5813
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2019-10-08 21:24:56 +0300

    task3

--- src/com/company/homework_1/Task_3.java
+++ src/com/company/homework_1/Task_3.java
@@ -1,6 +1,14 @@
 package com.company.homework_1;
 
 public class Task_3 {
-    int sum;
-    for
+    public static void main(String[] args) {
+    int sum=1;
+    int q=2;
+     for (int i = 0; i < 256; i++){
+         q=q*q;
+         sum=sum+q;
+     }
+        System.out.println("Addition: "+sum);
+
+    }
 }
