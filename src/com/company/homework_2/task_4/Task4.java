e20d7b1cc45718a013a94a72d5d8f4196324d2da
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-10 17:31:09 +0300

    Update Task4.java

--- src/com/company/homework_2/task_4/Task4.java
+++ src/com/company/homework_2/task_4/Task4.java
@@ -4,9 +4,10 @@ public class Task4 {
     public static void main(String[] args) {
         String firstArgument = args[0];//a
 
+        // a, b - так себе названия для переменных, придумай другие
         int a = Integer.valueOf(firstArgument);//to make my own values
         int b = Integer.valueOf(args[1]);
-        int c=0;//counter of multiplication
+        int c=0;//counter of multiplication - придумай хорошее ("говорящее") название переменной и комментарий не понадобится
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
