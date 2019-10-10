3c3fe5968f8861f77e3d7c9dfa913de292f7f881
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-10 13:12:42 +0300

    Update Task2.java

--- src/com/company/homework_2/task_2/Task2.java
+++ src/com/company/homework_2/task_2/Task2.java
@@ -2,9 +2,11 @@ package com.company.homework_2.task_2;
 
 public class Task2 {
     public static void main(String[] args) {
+        // a -> amoeba - гугл транслейт тебе в помощь
         int a=1;
         int iterationNumber=24;
         iterationNumber/=3;
+        // я думаю здесь имеет смысл не i++, а i = i+3, т.е. каждые 3 часа
         for (int i = 0; i < iterationNumber; i++) {
            a*=2;
         }
