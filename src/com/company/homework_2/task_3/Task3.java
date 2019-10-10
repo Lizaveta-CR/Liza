f76459f80ced36e39293a202ab58ac0899db5b96
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-10 13:17:53 +0300

    Update Task3.java

--- src/com/company/homework_2/task_3/Task3.java
+++ src/com/company/homework_2/task_3/Task3.java
@@ -3,10 +3,12 @@ package com.company.homework_2.task_3;
 public class Task3 {
     public static void main(String[] args) {
     int sum=0;
-     for (int i = 0; i < 256; i++){
+        // i = i * 2, но не забудь добавить 1
+     for (int i = 0; i < 256; i++){ // i <= 256
+         // у тебя будет так 0*0, 1*1, 2*2 -> 0 + 1 + 4, двойку потеряла
          i=i*i;//it's geometric progression
          sum=sum+i;
-
+// здесь пустая строка не нужна
      }
         System.out.println("Addition: "+sum);
 
