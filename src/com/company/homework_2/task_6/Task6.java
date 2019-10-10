37bad3f97d04ec79cd07a314c1a2abfed0d84a34
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-10 17:34:15 +0300

    Update Task6.java

--- src/com/company/homework_2/task_6/Task6.java
+++ src/com/company/homework_2/task_6/Task6.java
@@ -3,7 +3,8 @@ package com.company.homework_2.task_6;
 public class Task6 {
     public static void main(String[] args) {
         int iterationNumber=100;
-        int i=2;
+        int i=2; // придумай другое название переменной
+        // здесь имелось ввиду использование оператора '%' для выявления четного числа
         while(i<=iterationNumber){
             System.out.print(i+"\t");
             i+=2;
