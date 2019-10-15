2c03f68c3d08f024dbcdee106be094d1869136e9
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-15 20:57:20 +0300

    Update Task_2.java

--- src/com/company/homework_3/task_2/Task_2.java
+++ src/com/company/homework_3/task_2/Task_2.java
@@ -3,6 +3,7 @@ package com.company.homework_3.task_2;
 import java.util.Arrays;
 import java.util.Scanner;
 
+// форматируй код
 public class Task_2 {
     public static void main(String[] args) {
         double average = 0;
