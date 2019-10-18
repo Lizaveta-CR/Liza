e186c6ef90214bfff5601c35ca1d71661d1e2887
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-18 20:52:16 +0300

    Update Task_4.java

--- src/com/company/homework_4/task_4/Task_4.java
+++ src/com/company/homework_4/task_4/Task_4.java
@@ -4,7 +4,9 @@ import com.company.homework_4.task_1.Person;
 
 public class Task_4 {
     public static void main(String[] args) {
+        //people - множ число, сразу не подходит. people_1 -> unknown
         Person people_1 = new Person();
+        //helena
         Person people_2 = new Person("Helena", 22);
         people_1.setText("Hello from objects");
 
