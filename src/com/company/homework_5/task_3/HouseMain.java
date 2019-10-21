808627e9f3611a25bf2bdab54ee8e4dcd6ceb325
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-21 22:06:42 +0300

    Update HouseMain.java

--- src/com/company/homework_5/task_3/HouseMain.java
+++ src/com/company/homework_5/task_3/HouseMain.java
@@ -11,19 +11,24 @@ public class HouseMain {
             for (int j = i; j < houses.getArrayVacation().length; j++) {
                 System.out.println(houses.getArrayVacation()[i]);
                 //не понимаю,почему все равно возвращает "ссылочный вариант",хоть и сделана копия массива
+                // Ответ: для этого в классе VacationHome должен быть переопределен метод toString(), метод show() тогда можно будет убрать
             }
+            
+            // 1
             System.out.println("==City==");
             CityHouse cityHouse = new CityHouse(10, 100);
             cityHouse.show();
             System.out.println(cityHouse.getNumberOfFloors() + "\n" + cityHouse.getNumberOfTenants());
             cityHouse.turnOnHeating();
 
+            // 2
             System.out.println("==Village==");
             VacationHome vacationHome = new VacationHome(2, 4);
             vacationHome.show();
             System.out.println(vacationHome.getNumberOfFloors() + "\n" + vacationHome.getNumberOfTenants());
             vacationHome.turnOnHeating();
 
+            // 1 и 2 делают одно и тоже, думаю это хороший признак чтобы вынести эту логику в отдельный метод
         }
     }
 }
