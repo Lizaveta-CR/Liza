834eac25321cb6912609d7e0e372bae025520702
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 20:36:16 +0300

    Update Transmission.java

--- src/com/company/homework_6/task_2/Transmission.java
+++ src/com/company/homework_6/task_2/Transmission.java
@@ -3,21 +3,23 @@ package com.company.homework_6.task_2;
 public class Transmission {
     private boolean start = false;
 
+    // я думаю этот метод еще должен менять значение поля 'start'
     public void shutOffTheEngine() {
         if (!start) {
             System.out.println("The engine is off");
         } else {
             System.out.println("The engine is already off");
         }
-
+// пустая строка не нужна
     }
 
+    // я думаю этот метод еще должен менять значение поля 'start'
     public void turnOnTheEngine() {
         if (!start) {
             System.out.println("The engine is on");
         } else {
             System.out.println("The engine is already on");
         }
-
+// пустая строка не нужна
     }
 }
