dede99d42422ed3eabbaf2e1ed71493ff607dd45
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 20:45:02 +0300

    Update Car.java

--- src/com/company/homework_6/task_2/Car.java
+++ src/com/company/homework_6/task_2/Car.java
@@ -2,10 +2,12 @@ package com.company.homework_6.task_2;
 
 public class Car {
     private String number;
+// 'start' - это поле класса Engine, здесь ты у поля 'engine' можешь вызвать метод 'isStarted()' например   
     private boolean start = false;
     private Engine engine;
     private Transmission transmission;
 
+    // я бы Transmission & Engine тож передавал в конструкторе
     public Car(String number) {
         this.number = number;
         this.engine = new Engine();
@@ -13,6 +15,7 @@ public class Car {
     }
 
     public void gas() {
+        // лучше не использовать отрицание в if(), а поменять местами if/else
         if (!start) {
             System.out.println("Let's go!");
         } else {
@@ -36,6 +39,8 @@ public class Car {
     }
 
 
+    // по идее метод stop() должен понижать передачу до нулевой, а выключать двигатель - отдельный вызов метода
+    // start - если делая 'start = true' ты хотела выключить двигательно, то это нелогично
     public void stop() {
         if (!start) {
             start = true;
