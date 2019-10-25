a78546dc5edaaf2b398093c0afa0f31737a95af7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-25 20:39:19 +0300

    Update Engine.java

--- src/com/company/homework_6/task_2/Engine.java
+++ src/com/company/homework_6/task_2/Engine.java
@@ -1,16 +1,18 @@
 package com.company.homework_6.task_2;
 
+// у тебя в классе Engine лежит функционал класса Transmission
 public class Engine {
     private int currentGear;
 
     public void increaseGear() {
+        // 7 -> private static final int MAX_GEAR = 7;
         if (currentGear >= 0 && currentGear <= 7) {
             currentGear++;
         } else {
             System.out.println("Sorry, but i cant't do more");
         }
         System.out.println("Gear increased: " + currentGear);
-
+// пустая строка не нужна
     }
 
     public void decreaseGear() {
