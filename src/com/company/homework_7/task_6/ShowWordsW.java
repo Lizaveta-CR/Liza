6f96c8922a698d8f7244483cd246a5b3d5bb5566
Lizaveta-PAS <elizaveta.tsvirko@icloud.com>
2019-10-27 20:21:59 +0300

    done

--- src/com/company/homework_7/task_6/ShowWordsW.java
+++ src/com/company/homework_7/task_6/ShowWordsW.java
@@ -2,7 +2,7 @@ package com.company.homework_7.task_6;
 
 public class ShowWordsW {
     public static void main(String[] args) {
-        String[] words = new String[]{"width", "ellipse", "Wrong", "hyperbola"};
+        String[] words = {"width", "ellipse", "Wrong", "hyperbola"};
         for (int i = 0; i < words.length; i++) {
             if (words[i].startsWith("W") || words[i].startsWith("w")) {
                 System.out.println(words[i]);
