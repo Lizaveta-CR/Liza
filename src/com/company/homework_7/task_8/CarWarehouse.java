f36536207e49f54180ae5d23828a353b154c63ff
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 10:21:36 +0300

    Update CarWarehouse.java

--- src/com/company/homework_7/task_8/CarWarehouse.java
+++ src/com/company/homework_7/task_8/CarWarehouse.java
@@ -4,11 +4,11 @@ public class CarWarehouse {
     public static void main(String[] args) {
         CarEngine[] carEngines = {new CarEngine("1.4"), new CarEngine("1.8")};
         for (int i = 0; i < carEngines.length; i++) {
-            if (carEngines[i].getNumber().equals("1.4")) {
+            if (carEngines[i].getNumber().equals("1.4")) { // 1.4 / 2.0T - в переменную или константу
                 carEngines[i] = new CarEngine(carEngines[i].getNumber().replace("1.4", "2.0T"));
                 System.out.println(carEngines[i]);
             } else {
-                carEngines[i] = new CarEngine(carEngines[i].getNumber().replace("1.8", "1.8T"));
+                carEngines[i] = new CarEngine(carEngines[i].getNumber().replace("1.8", "1.8T")); // 1.8 / 1.8T - в переменную или константу
                 System.out.println(carEngines[i]);
             }
         }
