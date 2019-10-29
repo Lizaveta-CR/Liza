158fef92ffb501d0a0c1dc87f35a98377d817329
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 10:16:40 +0300

    Update ShowWordsW.java

--- src/com/company/homework_7/task_6/ShowWordsW.java
+++ src/com/company/homework_7/task_6/ShowWordsW.java
@@ -1,5 +1,6 @@
 package com.company.homework_7.task_6;
 
+// задание было отфильтровать, значит, на выходе должен быть массив соот-щий условию, а не просто вывести на экран
 public class ShowWordsW {
     public static void main(String[] args) {
         String[] words = {"width", "ellipse", "Wrong", "hyperbola"};
