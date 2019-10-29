53bbb77ddabfed27cb7f411cd9f99b3142ca5997
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-10-29 10:21:56 +0300

    Update Task_9.java

--- src/com/company/homework_7/task_9/Task_9.java
+++ src/com/company/homework_7/task_9/Task_9.java
@@ -1,5 +1,6 @@
 package com.company.homework_7.task_9;
 
+// +
 public class Task_9 {
     public static void main(String[] args) {
         String[] strings = {"Hello world", "Hello", "All", "Math", "Ellipse"};
