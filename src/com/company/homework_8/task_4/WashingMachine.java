62ce4381dcf5dbc05951bd80c2fc0bc796cc2e07
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-01 16:07:22 +0300

    Update WashingMachine.java

--- src/com/company/homework_8/task_4/WashingMachine.java
+++ src/com/company/homework_8/task_4/WashingMachine.java
@@ -14,16 +14,19 @@ public class WashingMachine {
         try {
             while (electricity) {
                 System.out.println("Electricity is on");
+                // 100 в константу
                 if (powerBlock.getPower() > 100) {
                     throw new WashingMachineException("Something is broken,I'm sorry...");
                 } else {
                     System.out.println("All right!");
                 }
+                // у тебя цикл всегда отработает только 1 раз либо будет исключение, если идея была такой, то ок
                 electricity = false;
             }
         } catch (WashingMachineException e) {
             throw new WashingMachineException("Something is broken,I'm sorry...");
         } finally {
+            // осталось только создать метод слива воды
             System.out.println("Water is flowing");
         }
     }
