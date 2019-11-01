b15c6610753b4adc5be3b92fb64e8c80a70109bd
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-01 15:49:12 +0300

    Update StringToNumber.java

--- src/com/company/homework_8/task_2/StringToNumber.java
+++ src/com/company/homework_8/task_2/StringToNumber.java
@@ -6,9 +6,10 @@ public class StringToNumber {
     public static void main(String[] args) {
         String string = null;
         try {
-            System.out.println("Please,enter numbers: ");
+            System.out.println("Please,enter numbers: "); // 'numbers:' вроде только 1
             Scanner scanner = new Scanner(System.in);
             string = scanner.next();
+            // Number -> number
             int Number = Integer.parseInt(string);
             System.out.println("Your number: " + Number);
         } catch (NumberFormatException e) {
