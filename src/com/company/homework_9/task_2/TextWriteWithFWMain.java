9b956a357f5c7568cb8d2e18064fdf79c436d077
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-05 21:15:53 +0300

    Update TextWriteWithFWMain.java

--- src/com/company/homework_9/task_2/TextWriteWithFWMain.java
+++ src/com/company/homework_9/task_2/TextWriteWithFWMain.java
@@ -7,21 +7,23 @@ public class TextWriteWithFWMain {
 
     public static void main(String[] args) {
         File file = new File(FILE_NAME);
+        // этот try в отдельный метод
         try {
             file.createNewFile();
         } catch (IOException e) {
             System.err.println("File can not be created" + e.getMessage());
         }
 
+       // этот try тоже
         try (FileWriter fileWriter = new FileWriter(FILE_NAME);
              BufferedWriter buff = new BufferedWriter(fileWriter)) {
-            int numberOFRec = 10000;
+            int numberOFRec = 10000; // numberOFRec -> Of
             String text = "Hello from Java";
             for (int i = 0; i < numberOFRec; i++) {
                 buff.append(" Hello from Java");
                 buff.write("\n");
             }
-
+// пустая строка не нужна
             buff.flush();
             System.out.println("File has been successfully writen");
         } catch (IOException e) {
