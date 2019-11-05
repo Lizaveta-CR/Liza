e872b9e00fcd0d10a749a9ba12e4419e4fbfe6f3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-05 21:17:21 +0300

    Update TextReadFOSMain.java

--- src/com/company/homework_9/task_3/TextReadFOSMain.java
+++ src/com/company/homework_9/task_3/TextReadFOSMain.java
@@ -11,7 +11,7 @@ public class TextReadFOSMain {
     public static void main(String[] args) {
         try (FileInputStream fis = new FileInputStream(FILE_PATH);
              BufferedInputStream buff = new BufferedInputStream(fis)) {
-            int string;
+            int string; // int string - выглядить странно
             while ((string = buff.read()) != -1) {
                 System.out.print((char) string);
             }
