9892e71cb757e4dd312487008e6b47d7a40d3e4e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-17 20:24:19 +0300

    Update RemoveAllElements.java

--- src/com/company/homework_11/task_3/RemoveAllElements.java
+++ src/com/company/homework_11/task_3/RemoveAllElements.java
@@ -3,6 +3,7 @@ package com.company.homework_11.task_3;
 import java.util.ArrayList;
 import java.util.List;
 
+// +
 public class RemoveAllElements {
     public static void main(String[] args) {
         List<String> tomFavoriteColors = new ArrayList<>();
