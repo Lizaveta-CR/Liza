3f215ca7630371d727452824d68ce97ea6eafa33
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-17 20:22:19 +0300

    Update PartyMain.java

--- src/com/company/homework_11/task_1/PartyMain.java
+++ src/com/company/homework_11/task_1/PartyMain.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// +
 public class PartyMain {
     public static void main(String[] args) {
         List<String> partyVisitors = new ArrayList<>();
