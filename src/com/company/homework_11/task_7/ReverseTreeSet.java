1829cb3167cc19479aceb7dcde58785bcf0570fd
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-17 20:37:56 +0300

    Update ReverseTreeSet.java

--- src/com/company/homework_11/task_7/ReverseTreeSet.java
+++ src/com/company/homework_11/task_7/ReverseTreeSet.java
@@ -8,7 +8,7 @@ public class ReverseTreeSet {
     public static void main(String[] args) {
         Comparator<Integer> customComparator = new CustomComparator();
         customComparator = customComparator.reversed();
-        Set<Integer> integers = new TreeSet<>(customComparator);
+        Set<Integer> integers = new TreeSet<>(customComparator); // customComparator -> Collections.reverseOrder()
         int numOfIteration = 100;
         for (int i = 0; i < numOfIteration; i++) {
             integers.add(i = i + i ^ 2);
