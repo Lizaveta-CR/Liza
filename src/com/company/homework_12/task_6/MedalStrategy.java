18e24a985337bc9c6806da2b87e79a2ae6f8a993
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-19 21:32:25 +0300

    Update MedalStrategy.java

--- src/com/company/homework_12/task_6/MedalStrategy.java
+++ src/com/company/homework_12/task_6/MedalStrategy.java
@@ -1,5 +1,6 @@
 package com.company.homework_12.task_6;
 
+// обычно добавляют к стратегии, если у енама внутри констант есть методы, думаю здесь можно просто назвать Medal
 public enum MedalStrategy {
     GOLD_MEDAL,
     SILVER_MEDAL,
