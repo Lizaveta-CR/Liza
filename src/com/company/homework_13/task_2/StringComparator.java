9f5846f3bb583cbca6abc996787cd83c65b16d8c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-22 19:27:35 +0300

    Update StringComparator.java

--- src/com/company/homework_13/task_2/StringComparator.java
+++ src/com/company/homework_13/task_2/StringComparator.java
@@ -2,6 +2,7 @@ package com.company.homework_13.task_2;
 
 import java.util.Comparator;
 
+// не стоит заводить новый класс, когда можно использовать базовый от java
 public class StringComparator implements Comparator<String> {
     @Override
     public int compare(String firstStr, String secondStr) {
