45723551bf74fa38422b253fda72dca1505cdb13
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-11-26 21:38:55 +0300

    Update StringMain.java

--- src/com/company/homework_14/task_4/StringMain.java
+++ src/com/company/homework_14/task_4/StringMain.java
@@ -2,6 +2,7 @@ package com.company.homework_14.task_4;
 
 import java.util.Scanner;
 
+// +
 public class StringMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
