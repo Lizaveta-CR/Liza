f29a55d0e423dd02a7519a2a3151a2a665476637
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-12-03 20:45:33 +0300

    Update ThreadMain.java

--- src/com/company/homework_16/task_1/ThreadMain.java
+++ src/com/company/homework_16/task_1/ThreadMain.java
@@ -4,5 +4,6 @@ public class ThreadMain {
     public static void main(String[] args) {
         MyThread myThread = new MyThread("MyThread");
         myThread.start();
+        // не стоит забывать про метод join()
     }
 }
